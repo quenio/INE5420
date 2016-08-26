7255aa1c3ebc205f00b46b4278010a54da4916da
Quenio dos Santos <quenio@me.com>
2016-08-26 12:21:35 -0300

    Using vector instead of tuple as the representation of the transform matrix.

--- graphics/coord.h
+++ graphics/coord.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <tuple>
+#include <vector>
 #include <cmath>
+#include <cassert>
 
 using namespace std;
 
@@ -30,32 +31,30 @@ private:
     double _x, _y;
 };
 
-// 2D transformations as a matrix
+// Transformations as a matrix
 class TransformMatrix
 {
 public:
-    typedef tuple<double, double, double> Column;
-
-    TransformMatrix(Column column1, Column column2)
-        : _column1(column1), _column2(column2), _column3({ 0.0, 0.0, 1.0 }) {}
-
-    TransformMatrix(Column column1, Column column2, Column column3)
-        : _column1(column1), _column2(column2), _column3(column3) {}
+    TransformMatrix(initializer_list<double> column1, initializer_list<double> column2, initializer_list<double> column3)
+        : _column1(column1), _column2(column2), _column3(column3)
+    {
+        assert(column1.size() == column2.size() == column3.size());
+    }
 
 private:
-    Column _column1, _column2, _column3;
+    vector<double> _column1, _column2, _column3;
 };
 
 // 2D translation as a matrix
 TransformMatrix translation(double dx, double dy)
 {
-    return TransformMatrix({ 1.0, 0.0, dx }, { 0.0, 1.0, dy });
+    return TransformMatrix({ 1.0, 0.0, dx }, { 0.0, 1.0, dy }, { 0.0, 0.0, 1.0 });
 }
 
 // 2D scaling as a matrix
 TransformMatrix scaling(double sx, double sy)
 {
-    return TransformMatrix({ sx, 0.0, 0.0 }, { 0.0, sy, 0.0 });
+    return TransformMatrix({ sx, 0.0, 0.0 }, { 0.0, sy, 0.0 }, { 0.0, 0.0, 1.0 });
 }
 
 // 2D rotation as a matrix
@@ -63,5 +62,5 @@ TransformMatrix rotation(double angle)
 {
     const double c = cos(angle);
     const double s = sin(angle);
-    return TransformMatrix({ c, s, 0.0 }, { -s, c, 0.0 });
+    return TransformMatrix({ c, s, 0.0 }, { -s, c, 0.0 }, { 0.0, 0.0, 1.0 });
 }
