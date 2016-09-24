2c182c3d5c6781d3ad1c620ec5ca5eeb4a548066
Quenio dos Santos <quenio@me.com>
2016-09-24 12:04:31 -0300

    #51 Using iterator in transform().

--- graphics/coord.h
+++ graphics/coord.h
@@ -212,8 +212,10 @@ inline Coord& operator *= (Coord &lhs, TransformMatrix matrix)
 // Transform coords according to m.
 inline void transform(const TransformMatrix m, list<reference_wrapper<Coord>> coords)
 {
-    for (Coord &coord: coords)
-        coord *= m;
+    typedef list<reference_wrapper<Coord>>::iterator it;
+
+    for (it i = coords.begin(); i != coords.end(); i++)
+        *i *= m;
 }
 
 // Translate coord by dx horizontally, dy vertically.
